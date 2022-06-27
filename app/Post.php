01ebff97461c6b34cd7adfdb147273528a5b52b8
Belloni-Marco-Camillo <96737807+Belloni-Marco-Camillo@users.noreply.github.com>
2022-06-27 09:19:54 +0200

    connectionDatabaseError

---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /**
+     * The tags that belong to the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
